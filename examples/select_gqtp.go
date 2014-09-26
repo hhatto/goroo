354bd3e70d99dffb4391c7413dc57b6106486cbb
Hideo Hattori <hattori-h@klab.com>
2014-09-26 10:27:56 +0900

    for debug

---
+++ examples/select_gqtp.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/hhatto/goroo"
+)
+
+func main() {
+	client := goroo.NewGroongaClient("gqtp", "localhost", 10043)
+	result, err := client.Call("select", map[string]string{"table": "Users"})
+	if err != nil {
+		fmt.Println("Call() error:", err)
+		return
+	}
+	fmt.Println(result)
+}
