b40e8e090e88ef84485c8e5311dbad30468c8d3b
135yshr <isago@on-s.co.jp>
2015-10-05 15:08:07 +0900

    add testcase table_list command
    table_list で取得できたテーブルの数が１件あることを確認するテストを追加

--- goroo_test.go
+++ goroo_test.go
@@ -39,6 +39,30 @@ func TestHttp_TableList_Empty_Success(t *testing.T) {
 	}
 }
 
+func TestHttp_TableList_Count1_Success(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		const body = "[[0,1444024497.318,0.0469999313354492],[[[\"id\",\"UInt32\"],[\"name\",\"ShortText\"],[\"path\",\"ShortText\"],[\"flags\",\"ShortText\"],[\"domain\",\"ShortText\"],[\"range\",\"ShortText\"],[\"default_tokenizer\",\"ShortText\"],[\"normalizer\",\"ShortText\"]],[256,\"TestGQTPClinet\",\"./markdown.db.0000100\",\"TABLE_HASH_KEY|PERSISTENT\",null,null,null,null]]]"
+		fmt.Fprintln(w, body)
+	}))
+	defer ts.Close()
+
+	u, err := url.Parse(ts.URL)
+	if err != nil {
+		t.Error(err)
+	}
+	client := NewHttpClient(u.Host)
+	res, err := client.Call("table_list", map[string]string{})
+	if err != nil {
+		t.Error(err)
+	}
+	if res.Status != 0 {
+		t.Errorf("status not zero.[%d]", res.Status)
+	}
+	if len(res.Body.([]interface{})) != 2 {
+		t.Errorf("body fail.[%s]", res.Body)
+	}
+}
+
 func TestHTTPClient(t *testing.T) {
 	t.SkipNow()
 	doGet = func(string) (*http.Response, error) {
