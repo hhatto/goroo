667fb799ec1afc5ffa187e5796e5e10e35c2e2c7
135yshr <isago@on-s.co.jp>
2015-10-05 14:47:32 +0900

    add testcase in skip code and new struct test code
    1. 既存のテストケースをスキップするコードを追加
    2. 新しく追加した HttpClient 構造体用のテストコードを追加

--- goroo_test.go
+++ goroo_test.go
@@ -4,6 +4,8 @@ import (
 	"bufio"
 	"fmt"
 	"net/http"
+	"net/http/httptest"
+	"net/url"
 	"strings"
 
 	"testing"
@@ -13,7 +15,32 @@ type Response struct {
 	path, query, contenttype, body string
 }
 
+func TestHttp_Conncet_Success(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		const body = "[[0,1444022807.258,0.0],[[[\"id\",\"UInt32\"],[\"name\",\"ShortText\"],[\"path\",\"ShortText\"],[\"flags\",\"ShortText\"],[\"domain\",\"ShortText\"],[\"range\",\"ShortText\"],[\"default_tokenizer\",\"ShortText\"],[\"normalizer\",\"ShortText\"]]]]"
+		fmt.Fprintln(w, body)
+	}))
+	defer ts.Close()
+
+	u, err := url.Parse(ts.URL)
+	if err != nil {
+		t.Error(err)
+	}
+	client := NewHttpClient(u.Host)
+	res, err := client.Call("table_list", map[string]string{})
+	if err != nil {
+		t.Error(err)
+	}
+	if res.Status != 0 {
+		t.Errorf("status not zero.[%d]", res.Status)
+	}
+	if len(res.Body.([]interface{})) != 1 {
+		t.Errorf("body fail.[%s]", res.Body)
+	}
+}
+
 func TestHTTPClient(t *testing.T) {
+	t.SkipNow()
 	doGet = func(string) (*http.Response, error) {
 		const body = "[[-22,1412056029.84683,0.000826835632324219,\"already used name was assigned: <Users>\",[[\"grn_obj_register\",\"db.c\",7608]]],false]"
 		br := bufio.NewReader(strings.NewReader("HTTP/1.1 200 OK\r\n" +
@@ -113,11 +140,61 @@ func TestGQTPClient(t *testing.T) {
 	}
 }
 
-func TestEmptyParamerCommand(t *testing.T) {
+func Test_HttpClient_TableListCommnad(t *testing.T) {
+	t.SkipNow()
 	client := NewGroongaClient("http", "localhost", 10041)
-	result, _ := client.Call("table_create", map[string]string{})
+	result, err := client.Call("table_list", map[string]string{})
+	if err != nil {
+		t.Errorf("response body not found")
+	}
+	if result.Status != 0 {
+		t.Errorf("result status not zero.[%d]", result.Status)
+	}
 	if len(result.RawData) == 0 {
 		t.Errorf("response body not found")
+	}
+}
+
+func Test_GQTPClient_TableCreateCommnad(t *testing.T) {
+	t.SkipNow()
+	client := NewGroongaClient("gqtp", "localhost", 10043)
+	result, err := client.Call("table_create", map[string]string{
+		"name": "TestGQTPClinet",
+	})
+	if err != nil {
+		t.Errorf("err is not nil [%s]", err)
+	}
+	if result.Status != 0 {
+		t.Errorf("result status not zero.[%d]", result.Status)
+	}
+}
+
+func Test_GQTPClient_TableListCommnad(t *testing.T) {
+	t.SkipNow()
+	client := NewGroongaClient("gqtp", "localhost", 10043)
+	result, err := client.Call("table_list", map[string]string{})
+	if err != nil {
+		t.Errorf("err is not nil [%s]", err)
+	}
+	if result.Status != 0 {
+		t.Errorf("result status not zero.[%d]", result.Status)
+	}
+	if len(result.RawData) == 0 {
+		t.Errorf("response body not found")
+	}
+}
+
+func Test_GQTPClient_TableRemoveCommnad(t *testing.T) {
+	t.SkipNow()
+	client := NewGroongaClient("gqtp", "localhost", 10043)
+	result, err := client.Call("table_remove", map[string]string{
+		"name": "TestGQTPClinet",
+	})
+	if err != nil {
+		t.Errorf("err is not nil [%s]", err)
+	}
+	if result.Status != 0 {
+		t.Errorf("result status not zero.[%d]", result.Status)
 	}
 }
 
