8fe92c061f28c0e9549a0d2b87688a2e24217bb7
135yshr <isago@on-s.co.jp>
2015-10-05 16:57:45 +0900

    Refactoring Initialize functions
    初期化処理を共通化
    
    1. テスト用サーバー作成メソッド(newServer)作成
    2. url.Parseのエラー処理を省略

--- http_client_test.go
+++ http_client_test.go
@@ -8,17 +8,18 @@ import (
 	"testing"
 )
 
-func TestHttp_TableList_Empty_Success(t *testing.T) {
-	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		const body = "[[0,1444022807.258,0.0],[[[\"id\",\"UInt32\"],[\"name\",\"ShortText\"],[\"path\",\"ShortText\"],[\"flags\",\"ShortText\"],[\"domain\",\"ShortText\"],[\"range\",\"ShortText\"],[\"default_tokenizer\",\"ShortText\"],[\"normalizer\",\"ShortText\"]]]]"
+func newServer(body string) *httptest.Server {
+	return httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintln(w, body)
 	}))
+}
+
+func TestHttp_TableList_Empty_Success(t *testing.T) {
+	const body = "[[0,1444022807.258,0.0],[[[\"id\",\"UInt32\"],[\"name\",\"ShortText\"],[\"path\",\"ShortText\"],[\"flags\",\"ShortText\"],[\"domain\",\"ShortText\"],[\"range\",\"ShortText\"],[\"default_tokenizer\",\"ShortText\"],[\"normalizer\",\"ShortText\"]]]]"
+	ts := newServer(body)
 	defer ts.Close()
 
-	u, err := url.Parse(ts.URL)
-	if err != nil {
-		t.Error(err)
-	}
+	u, _ := url.Parse(ts.URL)
 	client := newHttpClient(u.Host)
 	res, err := client.Call("table_list", map[string]string{})
 	if err != nil {
@@ -33,17 +34,13 @@ func TestHttp_TableList_Empty_Success(t *testing.T) {
 }
 
 func TestHttp_TableList_Count1_Success(t *testing.T) {
-	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		const body = "[[0,1444024497.318,0.0469999313354492],[[[\"id\",\"UInt32\"],[\"name\",\"ShortText\"],[\"path\",\"ShortText\"],[\"flags\",\"ShortText\"],[\"domain\",\"ShortText\"],[\"range\",\"ShortText\"],[\"default_tokenizer\",\"ShortText\"],[\"normalizer\",\"ShortText\"]],[256,\"TestGQTPClinet\",\"./markdown.db.0000100\",\"TABLE_HASH_KEY|PERSISTENT\",null,null,null,null]]]"
-		fmt.Fprintln(w, body)
-	}))
+	const body = "[[0,1444024497.318,0.0469999313354492],[[[\"id\",\"UInt32\"],[\"name\",\"ShortText\"],[\"path\",\"ShortText\"],[\"flags\",\"ShortText\"],[\"domain\",\"ShortText\"],[\"range\",\"ShortText\"],[\"default_tokenizer\",\"ShortText\"],[\"normalizer\",\"ShortText\"]],[256,\"TestGQTPClinet\",\"./markdown.db.0000100\",\"TABLE_HASH_KEY|PERSISTENT\",null,null,null,null]]]"
+	ts := newServer(body)
 	defer ts.Close()
 
-	u, err := url.Parse(ts.URL)
-	if err != nil {
-		t.Error(err)
-	}
+	u, _ := url.Parse(ts.URL)
 	client := newHttpClient(u.Host)
+
 	res, err := client.Call("table_list", map[string]string{})
 	if err != nil {
 		t.Error(err)
@@ -57,17 +54,13 @@ func TestHttp_TableList_Count1_Success(t *testing.T) {
 }
 
 func TestHttp_ColumnCreate_UserName_Success(t *testing.T) {
-	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		const body = "[[0,1444025635.392,0.00300002098083496],true]"
-		fmt.Fprintln(w, body)
-	}))
+	const body = "[[0,1444025635.392,0.00300002098083496],true]"
+	ts := newServer(body)
 	defer ts.Close()
 
-	u, err := url.Parse(ts.URL)
-	if err != nil {
-		t.Error(err)
-	}
+	u, _ := url.Parse(ts.URL)
 	client := newHttpClient(u.Host)
+
 	res, err := client.Call("table_list", map[string]string{})
 	if err != nil {
 		t.Error(err)
@@ -81,17 +74,13 @@ func TestHttp_ColumnCreate_UserName_Success(t *testing.T) {
 }
 
 func TestHttp_ColumnCreate_UserName_Fail(t *testing.T) {
-	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		const body = "[[-22,1444025814.842,0.0,\"already used name was assigned: <user_name>\",[[\"grn_obj_register\",\"db.c\",8966]]],false]"
-		fmt.Fprintln(w, body)
-	}))
+	const body = "[[-22,1444025814.842,0.0,\"already used name was assigned: <user_name>\",[[\"grn_obj_register\",\"db.c\",8966]]],false]"
+	ts := newServer(body)
 	defer ts.Close()
 
-	u, err := url.Parse(ts.URL)
-	if err != nil {
-		t.Error(err)
-	}
+	u, _ := url.Parse(ts.URL)
 	client := newHttpClient(u.Host)
+
 	res, err := client.Call("table_list", map[string]string{})
 	if err == nil {
 		t.Errorf("err is nil")
