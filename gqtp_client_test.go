9b0ea3b8ab94fce7f7b715564c966205de089583
135yshr <isago@on-s.co.jp>
2015-10-06 11:08:00 +0900

    del gqtpServer logic
    GQTPダミーサーバーに関する処理をgqtpserver_test.goに移動

--- gqtp_client_test.go
+++ gqtp_client_test.go
@@ -3,47 +3,8 @@ package goroo
 import (
 	"fmt"
 	"io"
-	"net"
 	"testing"
 )
-
-type gqtpServer struct {
-	Address  string
-	Listener net.Listener
-	handler  func(io.Writer, io.Reader)
-}
-
-func (g *gqtpServer) start() {
-	for {
-		conn, err := g.Listener.Accept()
-		if err != nil {
-			continue
-		}
-		g.handler(conn, conn)
-		conn.Close()
-	}
-}
-
-func (g *gqtpServer) Close() {
-	g.Listener.Close()
-}
-
-func newGqtpLocalListener() net.Listener {
-	l, err := net.Listen("tcp", "127.0.0.1:0")
-	if err != nil {
-		if l, err = net.Listen("tcp6", "[::1]:0"); err != nil {
-			panic(err)
-		}
-	}
-	return l
-}
-
-func newGqtpServer(handler func(io.Writer, io.Reader)) *gqtpServer {
-	l := newGqtpLocalListener()
-	gs := &gqtpServer{l.Addr().String(), l, handler}
-	go gs.start()
-	return gs
-}
 
 func TestGqtp_TableList_Empty_Success(t *testing.T) {
 	fmt.Println("create server")
