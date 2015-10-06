8be18b937cea66325d193f487e23bfc9d9b9f171
135yshr <isago@on-s.co.jp>
2015-10-06 09:15:05 +0900

    Initial commit
    GQTPプロトコル用のクライアントテストファイルを新しく追加

---
+++ gqtp_client_test.go
@@ -0,0 +1,17 @@
+package goroo
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestGqtp_TableList_Empty_Success(t *testing.T) {
+	client := newGqtpClient(fmt.Sprintf("%s:%d", "localhost", 10043))
+	res, err := client.Call("table_list", map[string]string{})
+	if err != nil {
+		t.Error(err)
+	}
+	if res.Status != 0 {
+		t.Errorf("status not zero.[%d]", res.Status)
+	}
+}
